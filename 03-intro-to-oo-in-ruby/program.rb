30670daa2923555542ac724faa569aed6a862f7c
Joseph Fraiser <jfrasier@SF-US04880-MBP13.local>
2018-10-10 13:21:33 -0500

    first commit

--- 03-intro-to-oo-in-ruby/program.rb
+++ 03-intro-to-oo-in-ruby/program.rb
@@ -1,9 +1,40 @@
-hero_first_name = 'Steve'
+require 'pry'
 
-hero_last_name = 'Rodgers'
+class Hero 
 
-hero_alias = 'Captain America'
+    def initialize (first_name, last_name, abilities)
+        @first_name = first_name
+        @last_name = last_name
+        @abilities = abilities
+    end
 
+    attr_accessor :first_name
+    attr_accessor :last_name
+    
+    def full_name
+    @first_name + ' ' + @last_name
+    end
+    
+    #Above code is the same as writing these lines of code.
+    # def first_name
+    #     @first_name
+    # end
+
+    # def first_name=(value)
+    #     @first_name = value
+    # end
+    
+    # def last_name
+    #     @last_name
+    # end
+
+    # def last_name=(value)
+    #     @last_name = value
+    # end
+
+end
+
+steve = Hero.new('Steve', 'Rodgers',
 hero_abilities = [
     {
         coolness: 5,
@@ -13,8 +44,8 @@ hero_abilities = [
         coolness: 10,
         name: 'Gravity Defying Shield'
     }
-]
+    ])
 
-def hero_full_name
-    hero_first_name + ' ' + hero_last_name
-end
+binding.pry
+
+
